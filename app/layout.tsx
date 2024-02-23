92826d9a7564877e5c6e1bd611fe9d9524ac7a12
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2024-02-23 10:43:20 -0800

    imported global font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./ui/globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import "@/app/ui/globals.css";
+import { inter } from "./ui/fonts";
 
 export const metadata: Metadata = {
   title: "Ryori Base",
